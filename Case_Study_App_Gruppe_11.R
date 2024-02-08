af8fe7dc7ed7a660dd331a129e95e8cd56f7e6b4
Ida Kossatz <ida.kossatz@campus.tu-berlin.de>
2024-02-08 16:02:58 +0100

    import der .txt ergänzen, noch nicht fertig

--- Case_Study_App_Gruppe_11.R
+++ Case_Study_App_Gruppe_11.R
@@ -5,74 +5,64 @@ library(dplyr)
 
 # -----------------------------------
 # Fahrzeug allgemein
-Fahrzeuge_OEM1_Typ11 <- read_csv(".//Data//Fahrzeug//Fahrzeuge_OEM1_Typ11.csv")
-Fahrzeuge_OEM1_Typ11_Fehleranalyse <- read_csv(".//Data//Fahrzeug//Fahrzeuge_OEM1_Typ11_Fehleranalyse.csv")
-Bestandteile_Fahrzeuge_OEM1_Typ11 <- read_csv2(".//Data//Fahrzeug//Bestandteile_Fahrzeuge_OEM1_Typ11.csv ")
+Fahrzeuge_OEM1_Typ11 <- read_csv(".\\Data\\Fahrzeug\\Fahrzeuge_OEM1_Typ11.csv")
+Fahrzeuge_OEM1_Typ11_Fehleranalyse <- read_csv(".\\Data\\Fahrzeug\\Fahrzeuge_OEM1_Typ11_Fehleranalyse.csv")
+Bestandteile_Fahrzeuge_OEM1_Typ11 <- read_csv2(".\\Data\\Fahrzeug\\Bestandteile_Fahrzeuge_OEM1_Typ11.csv ")
 
 # ------------------------------------
 # Karosserie
-Komponente_K4 <- read_csv2(".//Data//Komponente//Komponente_K4.csv")
-Bestandteile_Komponente_K4 <- read_csv2(".//Data//Komponente//Bestandteile_Komponente_K4.csv")
-Einzelteil_T30 <- read_csv(".//Data//Einzelteil//Einzelteil_T30.csv")
-# Einzelteil_T31.txt
-Einzelteil_T32 <- read_csv2(".//Data//Einzelteil//Einzelteil_T32.csv")
+Komponente_K4 <- read_csv2(".\\Data\\Komponente\\Komponente_K4.csv")
+Bestandteile_Komponente_K4 <- read_csv2(".\\Data\\Komponente\\Bestandteile_Komponente_K4.csv")
+Einzelteil_T30 <- read_csv(".\\Data\\Einzelteil\\Einzelteil_T30.csv")
+# !!! Einzelteil_T31 <- read_tsv(".\\Data\\Einzelteil\\Einzelteil_T31.txt")
+
+Einzelteil_T32 <- read_csv2(".\\Data\\Einzelteil\\Einzelteil_T32.csv")
 
 # --------------------------------------
 # Schaltung
 # Schaltgetriebe
-Komponente_K3SG1 <- read_csv(".//Data//Komponente//Komponente_K3SG1.csv")
-Bestandteile_Komponente_K3AG1 <- read_csv2(".//Data//Komponente//Bestandteile_Komponente_K3AG1.csv")
-Einzelteil_T21 <- read_csv2(".//Data//Einzelteil//Einzelteil_T21.csv")
-# Einzelteil_T22.txt
-Einzelteil_T23 <- read_csv2(".//Data//Einzelteil//Einzelteil_T23.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum.x))
+Komponente_K3SG1 <- read_csv(".\\Data\\Komponente\\Komponente_K3SG1.csv")
+Bestandteile_Komponente_K3AG1 <- read_csv2(".\\Data\\Komponente\\Bestandteile_Komponente_K3AG1.csv")
+Einzelteil_T21 <- read_csv2(".\\Data\\Einzelteil\\Einzelteil_T21.csv")
+# !!! Einzelteil_T22 <- read_tsv(".\\Data\\Einzelteil\\Einzelteil_T22.txt")
+Einzelteil_T23 <- read_csv2(".\\Data\\Einzelteil\\Einzelteil_T23.csv")
 
 # Automatikgetriebe
-Komponente_K3AG1 <- read_csv(".//Data//Komponente//Komponente_K3AG1.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum.x))
-Bestandteile_Komponente_K3SG1 <- read_csv2(".//Data//Komponente//Bestandteile_Komponente_K3SG1.csv")
+Komponente_K3AG1 <- read_csv(".\\Data\\Komponente\\Komponente_K3AG1.csv")
+Bestandteile_Komponente_K3SG1 <- read_csv2(".\\Data\\Komponente\\Bestandteile_Komponente_K3SG1.csv")
 # Einzelteil_T21.csv Doppelt
-# Einzelteil_T24.txt
-Einzelteil_T25 <- read_csv(".//Data//Einzelteil//Einzelteil_T25.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum))
+# !!! Einzelteil_T24 <- read_tsv(".\\Data\\Einzelteil\\Einzelteil_T24.txt")
+Einzelteil_T25 <- read_csv(".\\Data\\Einzelteil\\Einzelteil_T25.csv")
 
 # Sitze
-# Komponente_K2LE1s.txt
-Bestandteile_Komponente_K2LE1 <- read_csv2(".//Data//Komponente//Bestandteile_Komponente_K2LE1.csv")
-# Einzelteil_T11.txt
-Einzelteil_T14 <- read_csv2(".//Data//Einzelteil//Einzelteil_T14.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum))
-Einzelteil_T15 <- read_csv2(".//Data//Einzelteil//Einzelteil_T15.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum.x))
-# Komponente_K2LE1s.txt
+# !! Komponente_K2LE1 <- read_delim(".\\Data\\Komponente\\Komponente_K2LE1.txt", delim = "II")
+Bestandteile_Komponente_K2LE1 <- read_csv2(".\\Data\\Komponente\\Bestandteile_Komponente_K2LE1.csv")
+# !! Einzelteil_T11.txt <- read_tsv(".\\Data\\Einzelteil\\Einzelteil_T01.txt")
+
+Einzelteil_T14 <- read_csv2(".\\Data\\Einzelteil\\Einzelteil_T14.csv")
+Einzelteil_T15 <- read_csv2(".\\Data\\Einzelteil\\Einzelteil_T15.csv")
 # Bestandteile_Komponente_K2LE1 Doppelt
 # Einzelteil_T11.txt Doppelt
-Einzelteil_T12 <- read_csv2(".//Data//Einzelteil//Einzelteil_T12.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum.x))
-Einzelteil_T13 <- read_csv2(".//Data//Einzelteil//Einzelteil_T13.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum))
+Einzelteil_T12 <- read_csv2(".\\Data\\Einzelteil\\Einzelteil_T12.csv")
+Einzelteil_T13 <- read_csv2(".\\Data\\Einzelteil\\Einzelteil_T13.csv")
 
 # Motor
-Komponente_K1BE1 <- read_csv(".//Data//Komponente//Komponente_K1BE1.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum))
-Bestandteile_Komponente_K1BE1 <- read_csv2(".//Data//Komponente//Bestandteile_Komponente_K1BE1.csv")
-# Einzelteil_T01.txt
-# Einzelteil_T02.txt
-# Einzelteil_T03.txt
-Einzelteil_T04 <- read_csv2(".//Data//Einzelteil//Einzelteil_T04.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum))
+Komponente_K1BE1 <- read_csv(".\\Data\\Komponente\\Komponente_K1BE1.csv")
+Bestandteile_Komponente_K1BE1 <- read_csv2(".\\Data\\Komponente\\Bestandteile_Komponente_K1BE1.csv")
+# !! Einzelteil_T01 <- read_delim(".\\Data\\Einzelteil\\Einzelteil_T01.txt", delim = " | | ")
+# !! Einzelteil_T02 <- read_tsv(".\\Data\\Einzelteil\\Einzelteil_T01.txt")
+# !! Einzelteil_T03 <- read_delim(".\\Data\\Einzelteil\\Einzelteil_T01.txt", delim = "|")
+Einzelteil_T04 <- read_csv2(".\\Data\\Einzelteil\\Einzelteil_T04.csv")
 
-Komponente_K1DI1 <- read_csv(".//Data//Komponente//Komponente_K1DI1.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum.x))
-Bestandteile_Komponente_K1DI1 <- read_csv2(".//Data//Komponente//Bestandteile_Komponente_K1DI1.csv")
+Komponente_K1DI1 <- read_csv(".\\Data\\Komponente\\Komponente_K1DI1.csv")
+Bestandteile_Komponente_K1DI1 <- read_csv2(".\\Data\\Komponente\\Bestandteile_Komponente_K1DI1.csv")
 # Einzelteil_T01.txt Doppelt
 # Einzelteil_T02.txt Doppelt
-Einzelteil_T05 <- read_csv(".//Data//Einzelteil//Einzelteil_T05.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum.x))
-Einzelteil_T06 <- read_csv(".//Data//Einzelteil//Einzelteil_T06.csv") %>%
-  filter(!is.na(Fehlerhaft_Datum))
+Einzelteil_T05 <- read_csv(".\\Data\\Einzelteil\\Einzelteil_T05.csv")
+Einzelteil_T06 <- read_csv(".\\Data\\Einzelteil\\Einzelteil_T06.csv")
 
 # -----------------------------------------------------------------------------------
+# Filtern der Daten nach Teilen die fehlerhaft sind
 # -----------------------------------
 # Fahrzeug allgemein
 Fahrzeuge_OEM1_Typ11_sorted <- Fahrzeuge_OEM1_Typ11 %>%
